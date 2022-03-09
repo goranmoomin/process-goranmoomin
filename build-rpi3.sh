146ac410cbd1ea24dab5bba02083ddf3f10c3672
Sungbin Jo <goranmoomin@daum.net>
2022-03-09 14:55:54 +0900

    Update build process to my preference

--- build-rpi3.sh
+++ build-rpi3.sh
@@ -3,21 +3,21 @@
 ############################################
 # OSSPR2022                                #
 #                                          #
-# Builds your ARM64 kernel.                 #
+# Builds your ARM64 kernel.                #
 ############################################
 
 set -e
 
-# Did you install ccache?
-type ccache
+ARCH=arm64
+CC=clang
+CROSS_COMPILE='aarch64-linux-gnu-'
 
-# Some cleanups and setups
-rm -f arch/arm64/boot/Image
-rm -f arch/arm64/boot/dts/broadcom/*.dtb
-CROSS_COMPILER='ccache aarch64-linux-gnu-'
+if which ccache &> /dev/null; then
+	CC="ccache $CC"
+fi
 
 # Build .config
-make ARCH=arm64 CROSS_COMPILE="$CROSS_COMPILER" tizen_bcmrpi3_defconfig
+make ARCH="$ARCH" CC="$CC" CROSS_COMPILE="$CROSS_COMPILE" tizen_bcmrpi3_defconfig
 
 # Build kernel
-make ARCH=arm64 CROSS_COMPILE="$CROSS_COMPILER" -j$(nproc)
+make ARCH="$ARCH" CC="$CC" CROSS_COMPILE="$CROSS_COMPILE" -j$(nproc)
